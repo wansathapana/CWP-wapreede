1415a55fa80a0904a33e9a5772f9a25373733596
wansathapana <wansathapana@gmail.com>
2026-09-22 01:44:25 +0700

    add cell04

---
+++ cell04/ex02/calculator.py
@@ -0,0 +1,9 @@
+num1 = float(input("Give me the first number: "))
+num2 = float(input("Give me the second number: "))
+
+print("Thank you!")
+
+print(f"{num1:g} + {num2:g} = {num1 + num2:g}")
+print(f"{num1:g} - {num2:g} = {num1 - num2:g}")
+print(f"{num1:g} / {num2:g} = {num1 / num2:g}")
+print(f"{num1:g} * {num2:g} = {num1 * num2:g}")
